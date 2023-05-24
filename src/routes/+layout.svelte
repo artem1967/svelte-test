a2c2514b7374eb9a8e89131789ad18c324ebe158
Artem <artemrasputin583@gmail.com>
2023-05-24 15:41:42 +0800

    add pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,12 @@
     <div class="nav-container">
       <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
       <div class="nav-links">
-        {#each nav as link}
+      
+
+       {#if link.isShow}
         <a href={link.href} class="link">{link.title}</a>
-        {/each}
+       {/if}
+        
         
       </div>
     </div>
@@ -22,26 +25,31 @@
   </style>
     <script>
       const nav = [
-      {
-        title: 'Обо мне',
-        href: '/about'
-      },
-      {
-        title: 'Блог',
-        href: '/blog'
-      },
-      {
-        title: 'Портфолио',
-        href: '/project'
-      },
-      {
-        title: 'Контакты',
-        href: '/contact'
-      },
-      {
-        title: 'temp',
-        href: '/temp'
-      }
-    ]
+  {
+    title: 'Обо мне',
+    href: '/about',
+    isShow: true,
+  },
+  {
+    title: 'Блог',
+    href: '/blog',
+    isShow: true,
+  },
+  {
+    title: 'Портфолио',
+    href: '/project',
+    isShow: true,
+  },
+  {
+    title: 'Контакты',
+    href: '/contact',
+    isShow: true,
+  },
+  {
+    title: 'temp',
+    href: '/temp',
+    isShow: true,
+  },
+];
   </script>
   
